8ffc3803c3b5852bc2c8bf0ed6f25b3c9bc9cb1c
Michael Benedicto <mjybenedicto@gmail.com>
2026-05-06 13:54:10 +0900

    adjust to payments

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,6 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@mjybtickets/common';
 
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes';
-import { updateTicketRouter } from './routes/update';
-
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -19,11 +14,6 @@ app.use(
     })
 );
 app.use(currentUser);
-
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
